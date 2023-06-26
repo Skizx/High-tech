bf5805ae7a3be285b250e82bf93b673c866e14a5
KevinC <k.civiletti.gj@gmail.com>
2023-06-26 13:42:30 +0200

    Premier test live

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -30,65 +30,7 @@ const Contact = () => {
     <>
     {status ? (
        
-        <section id="contact" className="contact-block">
-            <div className="wrapper">
-            <div className="flex-center_content">
-                <h2 className="contact-title txt-center">Contact</h2>
-            </div>
-                <div className="contact-text">
-                    <p>Vous pouvez me contacter pour prendre rendez-vous, ainsi que tout renseignement en remplissant les champs ci-dessous ou par téléphone au xxxxxxx.</p>
-                </div>
-                <div className="horaire">
-                    <h2 className='txt-center'>Horaire & informations</h2>
-                    <div className="horaire-content">
-                        <div className="detail-content">
-                            <div className="detail-block">
-                                <div className="detail-title">
-                                    <FaHome />
-                                    <h3>Déplacement</h3>
-                                </div>
-                                <div className="detail-text">
-                                    <p>Tous les jours</p>
-                                </div>
-                            </div>
-                            <div className="detail-block">
-                                <div className="detail-title">
-                                    <FaPhoneSquareAlt />
-                                    <h3>Téléphone</h3>
-                                </div>
-                                <div className="detail-text">
-                                    <p>0673765906</p>
-                                </div>
-                            </div>
-                            <div className="detail-block">
-                                <div className="detail-title">
-                                    <FaMailBulk />
-                                    <h3>Email</h3>
-                                </div>
-                                <div className="detail-text">
-                                    <p>fany_22@gmail.com</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <form className='contact-form' ref={form} onSubmit={sendEmail}>  
-                  <label htmlFor="user_name1">Nom*</label>
-                  <input type="text" required='required' name="user_name" id='user_name1' />
-                  <label htmlFor="user_email1">Email*</label>
-                  <input type="email" required='required' name="user_email" id='user_email1' />
-                  <label htmlFor="message1">Message*</label>
-                  <textarea required="required" name="message" id='message1'/>
-                  <input type="submit" value="Envoyer" />
-                </form>
-            </div>
-            <div className="succes-message_block">
-                <p className='succes-message'>Message envoyé avec succés !</p>
-            </div>
-        </section>
-        
-    ) : (
-        <section id="contact" className="contact-block">
+<section id="contact" className="contact-block">
             <div className="wrapper">
             <div className="flex-center_content">
                 <h2 className="contact-title txt-center">Contact</h2>
@@ -100,7 +42,7 @@ const Contact = () => {
                     <div className="formulaire-group">
                         <div className="horaire-content">
                             <div className="detail-content">
-                    <h2 className='txt-center'>Horaire & informations</h2>
+                    <h2 className=''>Horaire & informations</h2>
                                 <div className="detail-block">
                                     <div className="detail-title">
                                         <FaHome />
@@ -130,15 +72,81 @@ const Contact = () => {
                                 </div>
                             </div>
                         </div>
-                            <form className='contact-form' ref={form} onSubmit={sendEmail}>  
-                            <label htmlFor="user_name1">Nom:*</label>
-                            <input type="text" required='required' name="user_name" placeholder='Anne' id='user_name1' />
-                            <label htmlFor="user_email1">Email:*</label>
-                            <input type="email" required='required' name="user_email" placeholder='exemple@gmail.com' id='user_email1' />
-                            <label htmlFor="message1">Message:*</label>
-                            <textarea required="required" name="message" placeholder='Bonjour, je souhaiterais prendre rendez-vous...' id='message1'/>
+                            <form className='contact-form' ref={form} onSubmit={sendEmail}>
+                            <div className="form-title">
+                                <h4>Formulaire de contact</h4>
+                            </div>  
+                            <label htmlFor="user_name1">Nom :*</label>
+                            <input type="text" required='required' name="user_name" placeholder='Prenom' id='user_name1' />
+                            <label htmlFor="user_email1">Email :*</label>
+                            <input type="email" required='required' name="user_email" placeholder='Adressmail@gmail.com' id='user_email1' />
+                            <label htmlFor="message1">Message :*</label>
+                            <textarea required="required" name="message" placeholder='Bonjour, je souhaiterais...' id='message1'/>
                             <input type="submit" value="Envoyer" />
-                                        </form>
+                            </form>
+                            </div>
+                    </div>
+            </div>
+            <div className="succes-message_block">
+                <p className='succes-message'>Message envoyé avec succés !</p>
+            </div>
+        </section>
+        
+    ) : (
+        <section id="contact" className="contact-block">
+            <div className="wrapper">
+            <div className="flex-center_content">
+                <h2 className="contact-title txt-center">Contact</h2>
+            </div>
+                <div className="contact-text">
+                    <p>Vous pouvez me contacter pour prendre rendez-vous, ainsi que tout renseignement en remplissant les champs ci-dessous ou par téléphone au 06 73 76 59 06.</p>
+                </div>
+                <div className="horaire">
+                    <div className="formulaire-group">
+                        <div className="horaire-content">
+                            <div className="detail-content">
+                    <h2 className=''>Horaire & informations</h2>
+                                <div className="detail-block">
+                                    <div className="detail-title">
+                                        <FaHome />
+                                        <h3>Déplacement</h3>
+                                    </div>
+                                    <div className="detail-text">
+                                        <p>Tous les jours</p>
+                                    </div>
+                                </div>
+                                <div className="detail-block">
+                                    <div className="detail-title">
+                                        <FaPhoneSquareAlt />
+                                        <h3>Téléphone</h3>
+                                    </div>
+                                    <div className="detail-text">
+                                        <p>06 73 76 59 06</p>
+                                    </div>
+                                </div>
+                                <div className="detail-block">
+                                    <div className="detail-title">
+                                        <FaMailBulk />
+                                        <h3>Email</h3>
+                                    </div>
+                                    <div className="detail-text">
+                                        <p>fany_22@hotmail.fr</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <form className='contact-form' ref={form} onSubmit={sendEmail}>
+                            <div className="form-title">
+                                <h4>Formulaire de contact</h4>
+                            </div>  
+                            <label htmlFor="user_name1">Nom :*</label>
+                            <input type="text" required='required' name="user_name" placeholder='Prenom' id='user_name1' />
+                            <label htmlFor="user_email1">Email :*</label>
+                            <input type="email" required='required' name="user_email" placeholder='Adressmail@gmail.com' id='user_email1' />
+                            <label htmlFor="message1">Message :*</label>
+                            <textarea required="required" name="message" placeholder='Bonjour, je souhaiterais...' id='message1'/>
+                            <input type="submit" value="Envoyer" />
+                            </form>
                             </div>
                     </div>
             </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,32 @@
 import './_footer.scss';
+import { AiOutlineInstagram , AiOutlineTwitter, AiOutlineCopyrightCircle } from 'react-icons/ai';
+import { FaFacebookF } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <>
         <section id="footer" className="footer-block">
             <div className="wrapper txt-center">
-                <h4 className='footer-title'>Developped by K.civiletti</h4>
+                <h4 className='footer-title'>Suivez-moi</h4>
+                <div className="footer-list_img">
+                    <img src="/images/30221.jpg" alt="" />
+                    <img src="/images/cahier-fournitures-beaute.jpg" alt="" />
+                    <img src="/images/tondeuses-ciseaux-plat.jpg" alt="" />
+                    <img src="/images/tondeuses-ciseaux-plat.jpg" alt="" />
+                    <img src="/images/30221.jpg" alt="" />
+                </div>
+                <div className="footer-list_icon">
+                    <a href="#">
+                        <AiOutlineInstagram />
+                    </a>
+                    <a href="#">
+                        <FaFacebookF />
+                    </a>
+                    <a href="#">
+                        <AiOutlineTwitter />
+                    </a>
+                </div>
+                <p>Copyright <AiOutlineCopyrightCircle /> 2023</p>
             </div>
         </section>
         </>
